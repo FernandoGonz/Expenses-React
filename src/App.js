b56828d3859c3e9acd19b6ebbbe8db155270692c
Fernando <fernando@agovest.com>
2022-03-10 20:12:11 -0600

    Implementing expense form to add new expense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const name = 'Fernandino palomino';
@@ -28,6 +29,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started :D {name}, nice to meet you</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
